46e9a239d5597e013c0603c6a217726d42a259b4
Arthur Tchamokouen <rodrigue.arthur@gmail.com>
2020-09-16 09:41:22 +0200

    bucket

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -10,3 +10,15 @@ resource "aws_s3_bucket" "my_s3_bucket" {
 		enabled = true
 	}
 }
+
+resource "aws_iam_user" "my_iam_user" {
+	name "rodart2"
+}
+
+output "my_bucket_details" {
+	value = aws_s3_bucket.my_s3_bucket
+}
+
+output "my_iam_user_details" {
+	value = aws_iam_user.my_iam_user
+}
