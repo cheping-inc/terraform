7bb554984d0ba8c4eb9b354b3c2118e5c3a2e37d
Arthur Tchamokouen <rodrigue.arthur@gmail.com>
2020-09-19 09:35:09 +0200

    ec2

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -1,24 +1,65 @@
 provider "aws" {
-	region = "us-east-1"
-	version = "~> 3.6.0"
+  region  = "us-east-1"
+  version = "~> 2.46"
 }
 
-# plan - execute 
-resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-s3-bucket-rodart-001"
-	versioning {
-		enabled = true
-	}
+resource "aws_default_vpc" "default" {
+
 }
 
-resource "aws_iam_user" "my_iam_user" {
-	name = "rodart2"
+resource "aws_security_group" "http_server_sg" {
+  name = "http_server_sg"
+  //vpc_id = "vpc-c49ff1be"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
 }
 
-output "my_bucket_details" {
-	value = aws_s3_bucket.my_s3_bucket
-}
+resource "aws_instance" "http_server" {
+  #ami                   = "ami-062f7200baf2fa504"
+  ami                    = data.aws_ami.aws_linux_2_latest.id
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
 
-output "my_iam_user_details" {
-	value = aws_iam_user.my_iam_user
+  //subnet_id              = "subnet-3f7b2563"
+  subnet_id = tolist(data.aws_subnet_ids.default_subnets.ids)[0]
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Welcome to in28minutes - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
+    ]
+  }
 }
