2f7a705248c35e0169c9f07716cb85e2d3e6a247
Arthur Tchamokouen <rodrigue.arthur@gmail.com>
2020-09-19 09:40:24 +0200

    ec2

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -39,14 +39,14 @@ resource "aws_security_group" "http_server_sg" {
 }
 
 resource "aws_instance" "http_server" {
-  #ami                   = "ami-062f7200baf2fa504"
-  ami                    = data.aws_ami.aws_linux_2_latest.id
+  ami                   = "ami-062f7200baf2fa504"
+  //ami                    = data.aws_ami.aws_linux_2_latest.id
   key_name               = "default-ec2"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
 
-  //subnet_id              = "subnet-3f7b2563"
-  subnet_id = tolist(data.aws_subnet_ids.default_subnets.ids)[0]
+  subnet_id              = "subnet-3f7b2563"
+  //subnet_id = tolist(data.aws_subnet_ids.default_subnets.ids)[0]
 
   connection {
     type        = "ssh"
