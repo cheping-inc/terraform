b2cbc17f657fd803a630a303f0936cbc2695bd45
Arthur Tchamokouen <rodrigue.arthur@gmail.com>
2020-09-19 09:57:11 +0200

    ec2

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "http_server_sg" {
 resource "aws_instance" "http_server" {
   ami                   = "ami-062f7200baf2fa504"
   //ami                    = data.aws_ami.aws_linux_2_latest.id
-  key_name               = "default-ec2"
+  key_name               = "default_ec2"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
 
